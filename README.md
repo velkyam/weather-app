f552570ad5d095a7dbd5ca1e4f89bd8152ea494a
velkyam <velkyam@hotmail.com>
2022-01-11 18:32:18 -0800

    weather app complete

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # weather-app
+The goal of this project was to create a weather web application. The website was build to provide current weather and near future weather forecast at any user input city. The app provides icon, temperature, wind, and humidity for today and 5 other days. Furthermore, it stores user input cities in local storage to make cities quickly accessible even if the site closes. 
+
+<img src="assets/images/screen.png" alt="desktop_screenshot_top" width="500"/>
+<img src="assets/images/screen1.png" alt="desktop_screenshot_top" width="500"/>
+
+
+Deployed Application Link:
+https://velkyam.github.io/weather-app/
+
+To edit this project:
+
+1. Create a new repository on your GitHub account and clone it to your computer.
+
+2. Copy this project to your local repository.
+
+3. Make changes.
+
+4. When you're ready to deploy, use the git add, git commit, git pull and git push commands to save and push your code to your GitHub repository.
+
+
+
+MIT License
+
+Copyright (c) [2021] [DatNguyen]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,8 @@ var fourdays = moment().add(4, 'days').format("L")
 var fivedays = moment().add(5, 'days').format("L")
 var dashBoard = document.querySelector("#dashBoard")
 var forecast = document.querySelector("#forecast")
-// var forecastcheck=document.querySelector("forecast").innerHTML
+var listCities = document.querySelector("#listCities")
+
 
 var week = [today,tomorrow,twodays,threedays,fourdays,fivedays]
 
@@ -22,77 +23,175 @@ var index=localStorage.getItem("index");
 if (index===null){
     index=0;
 }
-    
-    function renderTable(){
-
-        for (let i = 0; i < index; i++) {
-            var cityTab = document.querySelector("#city"+[i])
-                cityTab.textContent = localStorage.getItem("city"+[i])
-                cityTab.setAttribute("class","list-group-item list-item")
-                container.appendChild(cityTab)
-            }
-        }
-
-        searchBtn.addEventListener('click',function(event){
-            
 
 
-        var city = document.querySelector("#cityInput").value
-        var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey+"&units="+units;
-    
+
+function dash(){
+            var city = document.querySelector("#cityInput").value
+            var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey+"&units="+units;
+       
         fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-  
             localStorage.setItem("city"+[index],data.city.name)
             index++;
             localStorage.setItem("index",index)
 
-    
-            //today dashboard
-            var mainheading = document.querySelector("#heading")
-            var maintemp = document.querySelector("#temp")
-            var mainwind = document.querySelector("#wind")
-            var mainhum = document.querySelector("#hum")
-            mainheading.textContent = data.city.name +" (" + today + ")"
-            maintemp.textContent = "Temp: " + data.list[0].main.temp + " °F"
-            mainwind.textContent = "Wind: " + data.list[0].wind.speed + " MPH"
-            mainhum.textContent = "Humidity: " + data.list[0].main.humidity + "%"
-            //forecast title
-            var forecastTit=document.querySelector("#forecastTit")
-            forecastTit.textContent="5-Day Forecast:"
+        //today dashboard
+        dashBoard.setAttribute('class', 'dashboard')
+        var mainheading = document.createElement("h3")
+        var maintemp = document.createElement("p")
+        var mainwind = document.createElement("p")
+        var mainhum = document.createElement("p")
+        mainheading.textContent = data.city.name +" (" + today + ")"
+        maintemp.textContent = "Temp: " + data.list[0].main.temp + " °F"
+        mainwind.textContent = "Wind: " + data.list[0].wind.speed + " MPH"
+        mainhum.textContent = "Humidity: " + data.list[0].main.humidity + "%"
+        dashBoard.appendChild(mainheading)
+        dashBoard.appendChild(maintemp)
+        dashBoard.appendChild(mainwind)
+        dashBoard.appendChild(mainhum)
+        var icon = document.createElement("img")
+        icon.src = 'http://openweathermap.org/img/wn/'+data.list[0].weather[0].icon +'@2x.png'
+        mainheading.appendChild(icon)
+        
 
-            //forecast cards
-            for (let i = 1; i < 6; i++) {
-                
-                var card = document.createElement("div")
-                card.setAttribute("class","card")
-                            var heading = document.createElement("h1")
-                            var temp = document.createElement("p")
-                            var wind = document.createElement("p")
-                            var hum = document.createElement("p")
-                            heading.textContent = week[i]
-                            temp.textContent = "Temp: " + data.list[i].main.temp + " °F"
-                            wind.textContent = "Wind: " + data.list[i].wind.speed + " MPH"
-                            hum.textContent = "Humidity: " + data.list[i].main.humidity + "%"
-                            forecast.appendChild(card)
-                            card.appendChild(heading)
-                            heading.appendChild(temp)
-                            temp.appendChild(wind)
-                            wind.appendChild(hum)
-            }
         
-        renderTable();
-      
-        
-        })
-        
-        cityInput.value = "";
-    })
+
+        //forecast title
+        var forecastTit=document.querySelector("#forecastTit")
+        forecastTit.textContent="5-Day Forecast:"
+
+        //forecast cards
+        for (let i = 1; i < 6; i++) {
+                        
+            var card = document.createElement("div")
+            card.setAttribute("class","card")
+                        var heading = document.createElement("p")
+                        heading.setAttribute("style","font-weight: bold; font-size: 20px")
+                        var icon2 = document.createElement("img")
+                        icon2.src = 'http://openweathermap.org/img/wn/'+data.list[i].weather[0].icon +'.png'
+                        var temp = document.createElement("p")
+                        var wind = document.createElement("p")
+                        var hum = document.createElement("p")
+                        heading.textContent = week[i]
+                        temp.textContent = "Temp: " + data.list[i].main.temp + " °F"
+                        wind.textContent = "Wind: " + data.list[i].wind.speed + " MPH"
+                        hum.textContent = "Humidity: " + data.list[i].main.humidity + "%"
+                        forecast.appendChild(card)
+                        card.appendChild(heading)
+                        heading.appendChild(icon2)
+                        card.appendChild(temp)
+                        card.appendChild(wind)
+                        card.appendChild(hum)
+        }
     
     renderTable();
+  
+        
+    })
+            
+
+            dashBoard.innerHTML = ''
+            forecast.innerHTML = ''
+            listCities.innerHTML=''
+            
+        cityInput.value = "";
+    }
+    
+function renderTable(){
+
+        for (let i = 0; i < index; i++) {
+            var cityTab = document.createElement("button")
+                cityTab.setAttribute("id","#city"+[i])
+                cityTab.textContent = localStorage.getItem("city"+[i])
+                cityTab.setAttribute("class","list-group-item list-item")
+                listCities.appendChild(cityTab)
+                cityTab.addEventListener('click', function(event){
+                    console.log(localStorage.getItem("city"+[i]))
+                   
+                        var city = localStorage.getItem("city"+[i])
+                        var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey+"&units="+units;
+                   
+                    fetch(requestUrl)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log(data)
+                    //today dashboard
+                    dashBoard.setAttribute('class', 'dashboard')
+                    var mainheading = document.createElement("h2")
+                    var maintemp = document.createElement("p")
+                    var mainwind = document.createElement("p")
+                    var mainhum = document.createElement("p")
+                    mainheading.textContent = data.city.name +" (" + today + ")"
+                    maintemp.textContent = "Temp: " + data.list[0].main.temp + " °F"
+                    mainwind.textContent = "Wind: " + data.list[0].wind.speed + " MPH"
+                    mainhum.textContent = "Humidity: " + data.list[0].main.humidity + "%"
+                    dashBoard.appendChild(mainheading)
+                    dashBoard.appendChild(maintemp)
+                    dashBoard.appendChild(mainwind)
+                    dashBoard.appendChild(mainhum)
+                    var icon = document.createElement("img")
+                    icon.src = 'http://openweathermap.org/img/wn/'+data.list[0].weather[0].icon +'@2x.png'
+                    mainheading.appendChild(icon)
+            
+                    //forecast title
+                    var forecastTit=document.querySelector("#forecastTit")
+                    forecastTit.textContent="5-Day Forecast:"
+            
+                    //forecast cards
+                    for (let i = 1; i < 6; i++) {
+                        
+                        var card = document.createElement("div")
+                        card.setAttribute("class","card")
+                                    var heading = document.createElement("p")
+                                    heading.setAttribute("style","font-weight: bold; font-size: 20px")
+                                    var icon2 = document.createElement("img")
+                                    icon2.src = 'http://openweathermap.org/img/wn/'+data.list[i].weather[0].icon +'.png'
+                                    var temp = document.createElement("p")
+                                    var wind = document.createElement("p")
+                                    var hum = document.createElement("p")
+                                    heading.textContent = week[i]
+                                    temp.textContent = "Temp: " + data.list[i].main.temp + " °F"
+                                    wind.textContent = "Wind: " + data.list[i].wind.speed + " MPH"
+                                    hum.textContent = "Humidity: " + data.list[i].main.humidity + "%"
+                                    forecast.appendChild(card)
+                                    card.appendChild(heading)
+                                    heading.appendChild(icon2)
+                                    card.appendChild(temp)
+                                    card.appendChild(wind)
+                                    card.appendChild(hum)
+                    }
+                
+                renderTable();
+
+                })
+                        
+            
+                        dashBoard.innerHTML = ''
+                        forecast.innerHTML = ''
+                        listCities.innerHTML=''
+                        
+                    cityInput.value = "";
+                
+                })
+            }
+       
+        }
+
+searchBtn.addEventListener('click',function(event){
+    if (cityInput.value=== ""||cityInput.value === null){
+        alert("Must enter a city!")
+    } else{
+    dash()}
+     })        
+
+    
+renderTable();
 
 
 
